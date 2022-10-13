de3d0b7cd7b33065c95466e3154403e239d7a8c8
AlokSachan <alok.sachan.83@gmail.com>
2022-10-13 16:20:32 +0530

    add user controller

---
+++ src/main/java/com/alok/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.alok.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<String> getUserName(){
+        return new ResponseEntity<>("Hello from jenkins", HttpStatus.OK);
+    }
+}
